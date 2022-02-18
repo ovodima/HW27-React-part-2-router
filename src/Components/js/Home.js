a4139d9130147f319099d28719985bb261245b69
Dima <dima.tsymbal12345@gmail.com>
2022-02-18 22:39:24 +0200

    material UI add

--- src/Components/js/Home.js
+++ src/Components/js/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
@@ -7,10 +8,10 @@ export default function Home() {
     <h1>Home page</h1>
 
     <ul>
-      <li><Link to={'events'}>Events</Link></li>
-      <li><Link to={'doctors'}>Doctors</Link></li>
-      <li><Link to={'clients'}>Clients</Link></li>
-      <li><Link to={'appointments'}>Appointments</Link></li>
+      <li><Link to={'events'}><Button variant="contained" color="success" size="small">Event</Button></Link></li>
+      <li><Link to={'doctors'}><Button variant="contained" color="success" size="small">Doctors</Button></Link></li>
+      <li><Link to={'clients'}><Button variant="contained" color="success" size="small">Clients</Button></Link></li>
+      <li><Link to={'appointments'}><Button variant="contained" color="success" size="small">Appointments</Button></Link></li>
     </ul>
   </div>)
 };
